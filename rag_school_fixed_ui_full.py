76f7f305fefb2d5c4afb0e02a6aaab0e345b0186
Minsu Kim <38391712+mgrooo@users.noreply.github.com>
2026-03-08 08:34:58 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- rag_school_fixed_ui_full.py
+++ rag_school_fixed_ui_full.py
@@ -1427,7 +1427,6 @@ if st.session_state.pending_question:
 elif typed_prompt:
     prompt = typed_prompt
     auto_lang = detect_auto_language(typed_prompt)
-    st.session_state["answer_language"] = auto_lang
     answer_language = auto_lang
     ui = get_ui_text(answer_language)
 
@@ -1567,6 +1566,7 @@ if prompt:
             sources_text=sources_text,
             answer_preview=answer
         )
+
 
 
 
